80677081d70e1f817bcd451483a435cf9bc4cd8e
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2020-10-08 01:14:52 +0200

    :sparkles: Support for Win64 added

--- delphi/analyzer.py
+++ delphi/analyzer.py
@@ -1,8 +1,9 @@
 from binaryninja import BinaryReader, BinaryView
 from typing import Callable, List, Union
+import copy
 
 from .vmt import DelphiVMT
-from ..constants import VMTOffsets
+from .constants import VMTOffsets
 
 
 class DelphiAnalyzer(object):
@@ -10,15 +11,22 @@ class DelphiAnalyzer(object):
     TODO: Doc
     '''
 
-    def __init__(self, bv: BinaryView, delphi_version: int, start = -1, end = -1, offset_size = -1):
-        self._offset_size = offset_size if offset_size > 0 else bv.address_size
-        self._start = start if start >= 0 else self._default_start(bv)
-        self._end = end if end > 0 else self._default_end(bv)
+    def __init__(self, bv: BinaryView, delphi_version: int, offset_ptr_size = -1, start = -1, end = -1):
+        self._offset_ptr_size = offset_ptr_size if offset_ptr_size > 0 else bv.address_size
+        self._start = start if start >= 0 else bv.start
+        self._end = end if end > 0 else bv.end
         self._vmt_list: List[DelphiVMT] = []
         self._bv = bv
         self._br = BinaryReader(bv)
         self._delphi_version = delphi_version
-        self._vmt_offsets = VMTOffsets(delphi_version, self._offset_size)
+        self._vmt_offsets = VMTOffsets(delphi_version, self._offset_ptr_size)
+
+        # Not really sure about this but it's working on my test binary (Win64 Delphi v10.3)
+        # Need to check offsets for macOS
+        # I'll clean that later
+        if self._bv.view_type == 'PE' and self._offset_ptr_size == 8:
+            for x, y in self._vmt_offsets.__dict__.items():
+                setattr(self._vmt_offsets, x, y + -24)
 
 
     ## Properties
@@ -39,6 +47,10 @@ class DelphiAnalyzer(object):
     def vmt_list(self) -> List[DelphiVMT]:
         return self._vmt_list
 
+    @property
+    def vmt_offsets(self) -> VMTOffsets:
+        return copy.copy(self._vmt_offsets)
+
 
     ## Public API
 
@@ -49,10 +61,10 @@ class DelphiAnalyzer(object):
         while True:
             addy = self._get_possible_vmt()
 
-            if not addy:
+            if addy is None:
                 break
 
-            delphi_vmt = DelphiVMT(self._bv, self._delphi_version, addy, self._offset_size)
+            delphi_vmt = DelphiVMT(self._bv, self._delphi_version, addy, self, self._offset_ptr_size)
 
             if not delphi_vmt.is_valid:
                 continue
@@ -70,46 +82,26 @@ class DelphiAnalyzer(object):
 
 
     def _read_ptr(self) -> Union[None, int]:
-        if self._offset_size == 4:
+        if self._offset_ptr_size == 4:
             return self._br.read32()
-        elif self._offset_size == 8:
+        elif self._offset_ptr_size == 8:
             return self._br.read64()
 
 
     def _get_possible_vmt(self) -> int:
-        while self._br.offset <= self._end - self._offset_size - 1:
+        while self._br.offset <= self._end - self._offset_ptr_size - 1:
             begin = self._br.offset
 
             if not self._bv.is_valid_offset(begin):
-                self._br.seek_relative(self._offset_size)
+                self._br.seek_relative(self._offset_ptr_size)
                 continue
 
             class_vmt = self._read_ptr()
 
             if class_vmt is None:
                 # If BinaryReader can't read, it will not update the offset
-                self._br.seek_relative(self._offset_size)
+                self._br.seek_relative(self._offset_ptr_size)
                 continue
 
             if begin == class_vmt + self._vmt_offsets.cVmtSelfPtr:
                 return class_vmt
-
-
-    def _default_start(self, bv: BinaryView) -> int:
-        # if bv.view_type == 'ELF':
-        #     return bv.sections['.text'].start
-
-        # if bv.view_type == 'PE':
-        #     return bv.sections['CODE'].start
-
-        return bv.start
-
-
-    def _default_end(self, bv: BinaryView) -> int:
-        # if bv.view_type == 'ELF':
-        #     return bv.sections['.text'].end
-
-        # if bv.view_type == 'PE':
-        #     return bv.sections['CODE'].end
-
-        return bv.end
