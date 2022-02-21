965d5130747ea15b386d90c1d5ce308eaf2fed33
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2022-02-21 19:49:34 +0100

    :ambulance: Fix Structure builder type for new API
    Fix #1

--- bnhelpers.py
+++ bnhelpers.py
@@ -3,6 +3,12 @@ from binaryninja import BinaryView, Symbol, SymbolType, types, Type
 from .delphi_analyser import DelphiVMT
 from .constants import VMTFieldTypes
 
+## Backwards compatibility for API < 2500
+try:
+    StructureType = types.StructureBuilder
+except AttributeError:
+    StructureType = types.Structure
+
 
 class BNHelpers(object):
     @staticmethod
@@ -10,7 +16,11 @@ class BNHelpers(object):
         if not vmt.is_valid:
             return False
 
-        vmt_struct = types.Structure()
+        ## Backwards compatibility for API < 2500
+        try:
+            vmt_struct = types.StructureBuilder.create()
+        except AttributeError:
+            vmt_struct = types.Structure()
 
         if not BNHelpers._add_vmt_fields(bv, vmt, vmt_struct):
             return False
@@ -31,7 +41,7 @@ class BNHelpers(object):
     # Protected methods
 
     @staticmethod
-    def _add_vmt_fields(bv: BinaryView, vmt: DelphiVMT, out_struct: types.Structure) -> bool:
+    def _add_vmt_fields(bv: BinaryView, vmt: DelphiVMT, out_struct: StructureType) -> bool:
         address_size = bv.address_size
         field_types = VMTFieldTypes(bv.arch)
         vmt_offsets = vmt.vmt_offsets
@@ -52,7 +62,7 @@ class BNHelpers(object):
 
 
     @staticmethod
-    def _add_vmt_methods(bv: BinaryView, vmt: DelphiVMT, out_struct: types.Structure) -> bool:
+    def _add_vmt_methods(bv: BinaryView, vmt: DelphiVMT, out_struct: StructureType) -> bool:
         address_size = bv.address_size
 
         if not vmt.seek_to_vmt_offset(vmt.vmt_offsets.cVmtParent + address_size):
@@ -105,7 +115,7 @@ class BNHelpers(object):
     # Private methods
 
     @staticmethod
-    def __create_class_name_type(bv: BinaryView, vmt: DelphiVMT, out_struct: types.Structure) -> bool:
+    def __create_class_name_type(bv: BinaryView, vmt: DelphiVMT, out_struct: StructureType) -> bool:
         vmt_offsets = vmt.vmt_offsets
 
         if not vmt.seek_to_vmt_offset(vmt_offsets.cVmtClassName):
@@ -124,7 +134,12 @@ class BNHelpers(object):
         if class_name_len is None:
             return False
 
-        class_name_struct = types.Structure()
+        ## Backwards compatibility for API < 2500
+        try:
+            class_name_struct = types.StructureBuilder.create()
+        except AttributeError:
+            class_name_struct = types.Structure()
+
         class_name_struct.append(Type.int(1, False), 'length')
         class_name_struct.append(Type.array(Type.char(), class_name_len), 'value')
         struct_type = Type.structure_type(class_name_struct)
